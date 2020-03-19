dd8db65dc3cfda95666c9f1b0536451bda5e3eb8
Walter Heger <wheger2021@cosmoconsult.com>
2020-03-19 10:03:50 +0100

    two plants

--- src/esp/src/main.cpp
+++ src/esp/src/main.cpp
@@ -5,6 +5,15 @@
 #include <ESP8266HTTPClient.h>
 #include "config.h"
 
+// #define _Serial_
+#define _Humidity_
+// #define _Window_
+
+#define VoltagePin D1
+#define HumidityVccPin D2
+#define WindowVccPin D2
+#define WindowReadPin D5 // D3 has pull down resistor
+
 ESP8266WiFiMulti WiFiMulti;
 WiFiClient client;
 HTTPClient http;
@@ -13,23 +22,29 @@ String data;
 bool connectWiFi();
 void sendRequest();
 void createData();
-void goSleep();
+void goSleep(uint64_t time);
 int readAnalog(int vccPin);
 int readDigital(int vccPin, int readPin);
+void print(uint64_t value);
 
 void setup() {
+  #if defined(_Serial_)
   Serial.begin(115200);
   Serial.println();
-
-  if (!connectWiFi()) { delay(5000); ESP.restart(); }
+  #endif
 
   createData();
+  if (!connectWiFi()) { goSleep(60e6 * 30); }
   sendRequest();
+  #if defined(_Humidity_)
+  goSleep(60e6 * 60);
+  #endif
+  #if defined(_Window_)
+  goSleep(60e6 * 5);
+  #endif
 }
 
-void loop() {
-  goSleep();
-}
+void loop() {}
 
 bool connectWiFi() {
   WiFi.mode(WIFI_STA);
@@ -38,22 +53,31 @@ bool connectWiFi() {
   // WiFiMulti.addAP(ssid, password);
 
   int retry = 0;
+  #if defined(_Serial_)
   Serial.print("[WiFi] Connecting...");
+  #endif
   // while ((WiFiMulti.run() != WL_CONNECTED) && (retry++ < 10)) {
   while ((WiFi.status() != WL_CONNECTED) && (retry++ < 10)) {
     digitalWrite(2, LOW);
     delay(500);
     digitalWrite(2, HIGH);
     delay(500);
+    #if defined(_Serial_)
     Serial.print(".");
+    #endif
   }
+  #if defined(_Serial_)
   Serial.println();
+  #endif
 
   if (retry >= 11) {
+    #if defined(_Serial_)
     Serial.println("[WiFi] Could not connect to wifi.");
+    #endif
     return false;
   }
 
+  #if defined(_Serial_)
   Serial.print("[WiFi] SSID: ");
   Serial.println(WiFi.SSID());
   Serial.print("[WiFi] IP address: ");
@@ -64,23 +88,28 @@ bool connectWiFi() {
   Serial.println(WiFi.macAddress());
   Serial.print("[WiFi] ESP Chip ID: ");
   Serial.println(ESP.getChipId());
+  #endif
 
   return true;
 }
 
 void sendRequest() {
   if (http.begin(client, "http://api.walterheger.de/orangecedar/arduino.php")) {
+    #if defined(_Serial_)
     Serial.print("[HTTP] request...");
+    #endif
 
     http.addHeader("Content-Type", "text/json");
     int httpCode = http.POST(data);
 
+    #if defined(_Serial_)
     if (httpCode > 0) {
       Serial.println(httpCode);
       Serial.println(http.getString());
     } else {
       Serial.println("failed");
     }
+    #endif
     http.end();
   }
 }
@@ -91,31 +120,41 @@ void createData() {
   doc["rssi"] = WiFi.RSSI();
   doc["esp"] = ESP.getChipId();
   doc["time"] = millis();
-  // doc["voltage"] = readAnalog(D1); // hibiscus south
-  // doc["humidity"] = readAnalog(D2); // hibiscus south
-  doc["voltage"] = readAnalog(D1); // bathroom window
-  doc["window"] = readDigital(D2, D5); // bathroom window (D3 has pull down resistor)
+  #if defined(_Humidity_)
+  // doc["voltage"] = readAnalog(VoltagePin);
+  doc["humidity"] = readAnalog(HumidityVccPin);
+  #endif
+  #if defined(_Window_)
+  // doc["voltage"] = readAnalog(VoltagePin);
+  doc["window"] = readDigital(WindowVccPin, WindowReadPin);
+  #endif
   serializeJson(doc, data);
 }
 
-void goSleep() {
-  Serial.println("[ESP] Going to sleep...");
-  // ESP.deepSleep(60e6 * 1); // hibiscus south
-  ESP.deepSleep(60e6 * 5); // bathroom window
+void goSleep(uint64_t time) {
+  #if defined(_Serial_)
+  Serial.print("[ESP] Going to sleep for ");
+  // print(time);
+  Serial.print(time/60e6);
+  Serial.println("min...");
+  #endif
+  ESP.deepSleep(time);
   delay(100);
 }
 
 int readAnalog(int vccPin) {
   pinMode(vccPin, OUTPUT);
   digitalWrite(vccPin, HIGH);
-  delay(100);
+  delay(10);
   for (int i = 0; i < 10; i++) { analogRead(A0); delay(5); }
-  delay(50);
+  delay(20);
   int value = analogRead(A0);
+  #if defined(_Serial_)
   Serial.print("Pin ");
   Serial.print(vccPin);
   Serial.print(": ");
   Serial.println(value);
+  #endif
   digitalWrite(vccPin, LOW);
   return value;
 }
@@ -127,9 +166,26 @@ int readDigital(int vccPin, int readPin) {
   digitalWrite(vccPin, HIGH);
   int value = digitalRead(readPin);
   digitalWrite(vccPin, LOW);
+  #if defined(_Serial_)
   Serial.print("Pin ");
   Serial.print(vccPin);
   Serial.print(": ");
   Serial.println(value);
+  #endif
   return value;
+}
+
+void print(uint64_t value)
+{
+    const int NUM_DIGITS    = log10(value) + 1;
+
+    char sz[NUM_DIGITS + 1];
+   
+    sz[NUM_DIGITS] =  0;
+    for ( size_t i = NUM_DIGITS; i--; value /= 10)
+    {
+        sz[i] = '0' + (value % 10);
+    }
+   
+    Serial.print(sz);
 }
--- src/php/garmin.php
+++ src/php/garmin.php
@@ -6,19 +6,31 @@ $mysqli = getSqlInterface();
 $hibiscusSouthHeartbeat;
 $hibiscusSouthHumidity;
 $hibiscusSouthVoltage;
+$calatheaHeartbeat;
+$calatheaHumidity;
+$calatheaVoltage;
 $bathroomWindowHeartbeat;
 $bathroomWindowClosed;
 $bathroomWindowVoltage;
 
 $result = sqlCommand("SELECT * FROM `OrangeCedarSensors` WHERE `entryNo` IN (SELECT MAX(`entryNo`) FROM `OrangeCedarSensors` GROUP BY `mac`, `sensor`)");
 while ($sensor = $result->fetch_assoc()) {
-	if ($sensor["mac"] == "3C:71:BF:2A:AE:E4") {
+	if ($sensor["mac"] == "EC:FA:BC:63:1F:9E") {
 		$hibiscusSouthHeartbeat = date("H:i", $sensor["dateTime"]);
 		if ($sensor["sensor"] == 'humidity') {
 			$hibiscusSouthHumidity = $sensor["value"];
 		}
 		if ($sensor["sensor"] == 'voltage') {
 			$hibiscusSouthVoltage = $sensor["value"];
+		}
+	}
+	if ($sensor["mac"] == "3C:71:BF:2A:AE:E4") {
+		$calatheaHeartbeat = date("H:i", $sensor["dateTime"]);
+		if ($sensor["sensor"] == 'humidity') {
+			$calatheaHumidity = $sensor["value"];
+		}
+		if ($sensor["sensor"] == 'voltage') {
+			$calatheaVoltage = $sensor["value"];
 		}
 	}
 	if ($sensor["mac"] == "EC:FA:BC:61:F9:24") {
@@ -39,10 +51,15 @@ $result->close();
 closeSqlInterface();
 
 $output = array();
-$output[0]["name"] = sprintf("Hibiskus Lang %sh", $hibiscusSouthHumidity);
-$output[0]["value"] = sprintf("%s (%sv)", $hibiscusSouthHeartbeat, $hibiscusSouthVoltage);
-$output[1]["name"] = sprintf("Bad Fenster %s", $bathroomWindowClosed);
-$output[1]["value"] = sprintf("%s (%sv)", $bathroomWindowHeartbeat, $bathroomWindowVoltage);
+$output[0]["name"] = sprintf("Hibiskus %sh (%s)", $hibiscusSouthHumidity, $hibiscusSouthHeartbeat);
+$output[0]["value"] = "";
+// $output[0]["value"] = sprintf("%s (%sv)", $hibiscusSouthHeartbeat, $hibiscusSouthVoltage);
+$output[1]["name"] = sprintf("Calathea %sh (%s)", $calatheaHumidity, $calatheaHeartbeat);
+$output[1]["value"] = "";
+// $output[1]["value"] = sprintf("%s (%sv)", $hibiscusSouthHeartbeat, $hibiscusSouthVoltage);
+$output[2]["name"] = sprintf("Bad %s (%s)", $bathroomWindowClosed, $bathroomWindowHeartbeat);
+$output[2]["value"] = "";
+// $output[2]["value"] = sprintf("%s (%sv)", $bathroomWindowHeartbeat, $bathroomWindowVoltage);
 
 echoJson($output);
 ?>
